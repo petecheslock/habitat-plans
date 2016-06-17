c27074c4bda33378161b69214ed0c2a509b56101
Pete Cheslock <petecheslock@gmail.com>
2016-06-17 14:19:41 -0400

    Try to set more items in env flags to pass to start script

--- rabbitmq/plan.sh
+++ rabbitmq/plan.sh
@@ -1,5 +1,4 @@
 pkg_name=rabbitmq
-pkg_distname=$pkg_name
 pkg_origin=pete
 pkg_version=3.6.2
 pkg_maintainer="Pete Cheslock <petecheslock@gmail.com"
@@ -7,14 +6,16 @@ pkg_license=('rabbitmq')
 pkg_source=http://www.rabbitmq.com/releases/rabbitmq-server/v${pkg_version}/rabbitmq-server-generic-unix-${pkg_version}.tar.xz
 pkg_shasum=2de3b0c93ed6ae009648c855253602402246a0ae06e6aefa60637673fa8aa112
 pkg_filename=rabbitmq-server-generic-unix-${pkg_version}.tar.xz
-pkg_dirname=${pkg_distname}_server-${pkg_version}
-pkg_deps=(core/erlang/18.3)
-pkg_build_deps=()
+pkg_dirname=${pkg_name}_server-${pkg_version}
+pkg_deps=(core/sed core/coreutils core/erlang/18.3)
 
 do_build() {
   return 0
 }
 
 do_install() {
-  cp -r $HAB_CACHE_SRC_PATH/$pkg_dirname/* $pkg_prefix
+  cp -r $HAB_CACHE_SRC_PATH/${pkg_dirname}/* ${pkg_prefix}
+  #for DIR in ebin etc include plugins sbin share;
+    #do cp -r $HAB_CACHE_SRC_PATH/${pkg_dirname}/$DIR ${pkg_prefix};
+  #done
 }
